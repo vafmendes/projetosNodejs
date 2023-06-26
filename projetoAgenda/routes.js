3afac4d7fd22af83b89529105eec896918118f3a
vafmendes <vmendes667@gmail.com>
2023-06-25 23:51:06 -0300

    Rotas

---
+++ projetoAgenda/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const cadastroController = require('./src/controllers/cadastroController');
+
+//Rotas da home
+route.get('/', homeController.index);
+
+//Rotas de login
+route.get('/login/index', loginController.index);
+
+//Rotas de cadastro
+route.get('/cadastro', cadastroController.cadastro);
+
+
+module.exports = route;
