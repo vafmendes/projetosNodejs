1c87a33468d51772af841161d32b7c38e532483d
vafmendes <vmendes667@gmail.com>
2023-06-25 23:52:26 -0300

    Carregamento das páginas e execução do servidor

---
+++ projetoAgenda/server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true})
+     .then(()=> {
+        console.log('base de dados conectada');
+        app.emit('pronto')
+     }).catch(e => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+// const helmet = require('helmet');
+const routes = require('./routes');
+const path = require('path');
+const csurf = require('csurf');
+const {middewareGlobal, checkCsrfError, csrfMiddleware} = require('./src/middlewares/middleware');
+
+
+//Carregando páginas
+
+// app.use(helmet());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOption = session({
+    secret: 'secrets_sessions',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 20000,
+        httpOnly: true
+    }
+});
+app.use(sessionOption);
+app.use(flash());
+
+app.set('views', path.resolve(__dirname,'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(csurf());
+app.use(middewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on('pronto', ()=>{
+    app.listen(3000, () =>{
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+    });
+});
+
+
