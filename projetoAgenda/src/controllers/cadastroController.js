25335cc4725659aa62c4bf2a29a36b891049f805
vafmendes <vmendes667@gmail.com>
2023-09-08 00:19:15 -0300

    Validação final para a base de dados dos valores cadastrados

--- projetoAgenda/src/controllers/cadastroController.js
+++ projetoAgenda/src/controllers/cadastroController.js
@@ -1,4 +1,28 @@
+const Cadastro = require('../models/CadastroModel');
+
 exports.cadastro = (req, res)=>{
     res.render('../views/cadastro');
     
+}
+
+exports.register = async (req, res)=>{
+    const cadastro = new Cadastro(req.body);
+    try{
+        await cadastro.register();
+        if(cadastro.errors.length > 0){
+            req.flash('errors', cadastro.errors);
+            req.session.save(()=>{
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso');
+            req.session.save(()=>{
+                return res.redirect('back');
+            });
+    }catch(e){
+        console.log(e);
+        return res.render('../views/includes/404');
+    }
 }
