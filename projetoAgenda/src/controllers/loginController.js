7d6a17203f6ed07e5a1c177d72b614a8aa51e27f
vafmendes <vmendes667@gmail.com>
2023-09-16 00:49:21 -0300

    Configurado o controle de login

--- projetoAgenda/src/controllers/loginController.js
+++ projetoAgenda/src/controllers/loginController.js
@@ -1,4 +1,34 @@
+const Login = require('../models/CadastroModel');
+
 exports.index= (req, res)=>{
     res.render('../views/login');
+}
+
+exports.login = async (req, res)=>{
+    const login = new Login(req.body);
+    try{
+        await login.login();
+        if(login.errors.length > 0){
+            req.flash('errors', login.errors);
+            req.session.save(()=>{
+                return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Seja bem vindo');
+        req.session.user = login.user;
+            req.session.save(()=>{
+                return res.redirect('/');
+            });
+    }catch(e){
+        console.log(e);
+        return res.render('../views/includes/404');
+    }
+};
+
+exports.logout = function(req, res){
+    req.session.destroy();
+    res.redirect('/');
 }
 
