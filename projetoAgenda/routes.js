97203f985684f9782832eaedcdf13e1779be5564
vafmendes <vmendes667@gmail.com>
2023-09-18 16:22:43 -0300

    Criada a rota 'contatos'

--- projetoAgenda/routes.js
+++ projetoAgenda/routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const cadastroController = require('./src/controllers/cadastroController');
+const contatosController = require('./src/controllers/contatosController');
 
 //Rotas da home
 route.get('/', homeController.index);
@@ -15,6 +16,9 @@ route.get('/login/logout', loginController.logout);
 //Rotas de cadastro
 route.get('/cadastro', cadastroController.cadastro);
 route.post('/cadastro/register', cadastroController.register);
+
+//Rota de contatos
+route.get('/contatos/', contatosController.contato);
 
 
 module.exports = route;
