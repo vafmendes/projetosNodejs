3a3490112e0a345a817caa3e91c8eb8356adc954
vafmendes <vmendes667@gmail.com>
2023-09-22 08:14:30 -0300

    Criada função loginRequired, que restringe o acesso à página contatos pela url

--- projetoAgenda/src/middlewares/middleware.js
+++ projetoAgenda/src/middlewares/middleware.js
@@ -19,7 +19,7 @@ exports.outroMiddleware = (req, res, next)=>{
 // }
 
 exports.checkCsrfError = (err, req, res, next) =>{
-    if(err){
+    if(err && err.code){
      return res.render('../views/includes/404');
     }
  
@@ -29,5 +29,17 @@ exports.checkCsrfError = (err, req, res, next) =>{
 
 exports.csrfMiddleware = (req, res, next) =>{
     res.locals.csrfToken = req.csrfToken();
+    next();
+}
+
+exports.loginRequired = (req, res, next) =>{
+    if(!req.session.user){
+        req.flash('errors', 'Você precisa fazer login');
+        req.session.save(()=>{
+            res.redirect('/');
+        });
+        return;
+    }
+
     next();
 }
